616b7a53bc80d0fc2d79b2aa157ba6afc7d9e07f
Arturas Aleksandrauskas <Arturas.Aleksandrauskas@contextvision.se>
2020-04-27 17:32:39 +0200

    no while

--- server.py
+++ server.py
@@ -5,45 +5,44 @@ import time
 import picamera
 from hidden import IP
 
-while True:
+try:
+    server_socket = socket.socket()
+    server_socket.bind((IP, 8000)) 
+    server_socket.listen(0)
+
+    # Accept a single connection and make a file-like object out of it
+    connection = server_socket.accept()[0].makefile('wb')
+    camera = picamera.PiCamera()
+    camera.vflip = True
+    camera.resolution = (1920, 1080)
+    # Start a preview and let the camera warm up for 2 seconds
+    camera.start_preview()
+    time.sleep(2)
+
+    # Note the start time and construct a stream to hold image data
+    # temporarily (we could write it directly to connection but in this
+    # case we want to find out the size of each capture first to keep
+    # our protocol simple)
+    stream = io.BytesIO()
+    for foo in camera.capture_continuous(stream, 'jpeg'):
+        # Write the length of the capture to the stream and flush to
+        # ensure it actually gets sent
+        connection.write(struct.pack('<L', stream.tell()))
+        connection.flush()
+        # Rewind the stream and send the image data over the wire
+        stream.seek(0)
+        connection.write(stream.read())
+        # Reset the stream for the next capture
+        stream.seek(0)
+        stream.truncate()
+    # Write a length of zero to the stream to signal we're done
+    connection.write(struct.pack('<L', 0))
+finally:
     try:
-        server_socket = socket.socket()
-        server_socket.bind((IP, 8000)) 
-        server_socket.listen(0)
-
-        # Accept a single connection and make a file-like object out of it
-        connection = server_socket.accept()[0].makefile('wb')
-        camera = picamera.PiCamera()
-        camera.vflip = True
-        camera.resolution = (1920, 1080)
-        # Start a preview and let the camera warm up for 2 seconds
-        camera.start_preview()
-        time.sleep(2)
-
-        # Note the start time and construct a stream to hold image data
-        # temporarily (we could write it directly to connection but in this
-        # case we want to find out the size of each capture first to keep
-        # our protocol simple)
-        stream = io.BytesIO()
-        for foo in camera.capture_continuous(stream, 'jpeg'):
-            # Write the length of the capture to the stream and flush to
-            # ensure it actually gets sent
-            connection.write(struct.pack('<L', stream.tell()))
-            connection.flush()
-            # Rewind the stream and send the image data over the wire
-            stream.seek(0)
-            connection.write(stream.read())
-            # Reset the stream for the next capture
-            stream.seek(0)
-            stream.truncate()
-        # Write a length of zero to the stream to signal we're done
-        connection.write(struct.pack('<L', 0))
-    finally:
-        try:
-            connection.close()
-        except:
-            print("cant close connection")
-        try:
-            server_socket.close()
-        except:
-            print("cant close socket")
+        connection.close()
+    except:
+        print("cant close connection")
+    try:
+        server_socket.close()
+    except:
+        print("cant close socket")
